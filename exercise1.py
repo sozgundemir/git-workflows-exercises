2355a9066e2167e7a9575f095a94335a78917ef0
Sinan Ozgun Demir <sozgundemir@gmail.com>
2022-10-04 15:32:27 +0200

    task a in exercise 1 is completed

--- exercise1.py
+++ exercise1.py
@@ -27,7 +27,8 @@ class Vector:
 def test_vector_index_access() -> None:
     for index in [0, 1, 2, 3]:
         reference = [float(i) for i in range(4)]
-        vector = Vector(reference)
+        # vector = Vector(reference)
+        vector = Vector(reference.copy())
         assert all(reference[i] == vector[i] for i in range(4))
         vector[index] = 42.0
         assert vector[index] == 42.0
